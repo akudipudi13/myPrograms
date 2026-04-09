c7c51b01ee75ee2c8e6658c9bc7e53be76c7c21d
akudipudi13 <akthesuperboy@gmail.com>
2026-04-09 16:26:56 -0700

    Add files via upload
    This is the updated versions of my old code.

---
+++ countCharTest.py
@@ -0,0 +1,31 @@
+from countChar import counting
+
+import pytest
+
+@pytest.mark.parametrize("char, str, expected", [
+    ("a", "banana", 3),
+    ("a", "bAnanA", 1),
+    ("A", "banAna", 1),
+    ("a", "BANANA", 0),
+    ("A", "banana", 0),
+    ("Z", "banana", 0),
+    ("b", "b", 1),
+])
+def test_countCharacters(char, str, expected):
+    assert counting(char, str) == expected
+
+if __name__ == "__main__":
+    print("Running pytest test cases...\n")
+    pytest.main([__file__])
+
+
+
+
+
+
+
+
+
+
+
+
