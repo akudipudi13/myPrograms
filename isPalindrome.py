a72a0e29b892d675c0a57acb73250dc1c5e8882b
Akhil Kudipudi <akthesuperboy@gmail.com>
2026-04-22 18:52:30 -0700

    palindrome test and program

---
+++ isPalindrome.py
@@ -0,0 +1,9 @@
+def checkPalindrome(strInput):
+    length = len(strInput)
+    for i in range(length%2):
+        if strInput[i] != strInput[length - (i + 1)]:
+            return False
+    return True
+
+strInput = input("String: ")
+print(checkPalindrome(strInput))
